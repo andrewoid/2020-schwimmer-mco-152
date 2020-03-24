9d941deb4b8e5d59d75133b50246e09bc258f0f5
Schwimmer, Andrew <Andrew.Schwimmer@mlb.com>
2020-03-24 19:48:52 -0400

    added image and animation

--- src/schwimmer/shapes/ShapeComponent.java
+++ src/schwimmer/shapes/ShapeComponent.java
@@ -1,4 +1,71 @@
 package schwimmer.shapes;
 
-public class ShapeComponent {
+import javax.swing.*;
+import java.awt.*;
+
+public class ShapeComponent extends JComponent {
+
+    private int x = 0;
+    private int rotation = 0;
+    private String shape = "Star";
+
+    public ShapeComponent() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (true) {
+                    repaint();
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+        int centerX = width / 2;
+        int centerY = height / 2;
+
+        g.translate(centerX+x, centerY);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.rotate(Math.toRadians(rotation));
+        x++;
+        rotation++;
+
+        if (shape.equals("Star")) {
+            drawStar(g);
+        }
+        else if (shape.equals("House")) {
+            drawHouse(g);
+        }
+    }
+
+    private void drawHouse(Graphics g) {
+
+    }
+
+    private void drawStar(Graphics g) {
+        g.drawLine(-50, -50, +50, -50);
+        g.drawLine(+50, -50, 0, +50);
+        g.drawLine(0, +50, -50, -50);
+
+        g.drawLine(-50, +25, +50, +25);
+        g.drawLine(+50, +25, 0, -75);
+        g.drawLine(0, -75, -50, +25);
+    }
+
+    public void setShapeHouse() {
+        shape = "House";
+        repaint();
+    }
 }
--- src/schwimmer/shapes/ShapesFrame.java
+++ src/schwimmer/shapes/ShapesFrame.java
@@ -1,4 +1,31 @@
 package schwimmer.shapes;
 
-public class ShapesFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class ShapesFrame extends JFrame {
+
+    private ShapeComponent shapeComponent;
+
+    public ShapesFrame() {
+        setSize(800, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("SHAPES");
+
+        setLayout(new BorderLayout());
+
+        shapeComponent = new ShapeComponent();
+        add(shapeComponent, BorderLayout.CENTER);
+
+        ImageIcon houseImage = new ImageIcon("house.jpg");
+        JButton houseButton = new JButton(houseImage);
+        add(houseButton, BorderLayout.WEST);
+        houseButton.addActionListener(actionEvent -> shapeComponent.setShapeHouse());
+
+    }
+
+    public static void main(String[] args) {
+        new ShapesFrame().setVisible(true);
+    }
+
 }
